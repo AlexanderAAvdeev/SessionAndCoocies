a1f284d2fb3f61e9674f0343f75e2244cadf5d29
Alexander Avdeev <2035-a.a.avdeev@mpt.ru>
2023-04-26 16:09:17 +0300

    This file being used for test Java program Session and Cookies

---
+++ src/main/java/com/example/sessionandcoocies/SessionAndCoociesApplication.java
@@ -0,0 +1,13 @@
+package com.example.sessionandcoocies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionAndCoociesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SessionAndCoociesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sessionandcoocies/SessionAndCoociesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sessionandcoocies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionAndCoociesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
